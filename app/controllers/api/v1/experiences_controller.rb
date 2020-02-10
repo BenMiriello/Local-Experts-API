ee8072c2e3f88daa6f299fe5309e2e12cdd2d167
BenMiriello <benmiriello@gmail.com>
2020-02-09 23:32:09 -0500

    api is working and experience location now renders in test embedded google maps

--- app/controllers/api/v1/experiences_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::V1::ExperiencesController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show, :create, :update, :destroy]
-  resources :locations, only: [:show]
-  resources :experiences, only: [:show]
-  resources :events, only: [:show]
-  resources :favorites, only: [:show, :create, :update, :destroy]
-  resources :saves, only: [:show, :create, :update, :destroy]
-  resources :trips, only: [:show, :create, :update, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show, :create, :update, :destroy]
+      resources :locations, only: [:show]
+      resources :experiences, only: [:show]
+      resources :events, only: [:show]
+      resources :favorites, only: [:show, :create, :update, :destroy]
+      resources :saves, only: [:show, :create, :update, :destroy]
+      resources :trips, only: [:show, :create, :update, :destroy]
+    end
+  end
 
 end
 
